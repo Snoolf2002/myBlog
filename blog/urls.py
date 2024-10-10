3e1f491d2d90b2dd8170e941d5f69f9dd2084138
Snoolf2002 <abdurakhmonovjaloliddin2002@gmail.com>
2024-10-10 14:21:14 +0500

    added views, urls, and form for Blog templates

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from .views import BlogAPIView
+from .views import BlogAPIView, BlogListView, BlogDetailView, BlogCreateView, BlogUpdateView, BlogDeleteView
 
 urlpatterns = [
-    path('blogs/', BlogAPIView.as_view(), name='blog-list'),
-    path('blogs/<int:pk>/', BlogAPIView.as_view(), name='blog-detail'),  
+    path('api/blogs/', BlogAPIView.as_view(), name='api-blog-list'),
+    path('api/blogs/<int:pk>/', BlogAPIView.as_view(), name='api-blog-detail'),
+    path('', BlogListView.as_view(), name='blog-list'),
+    path('blog/<int:pk>/', BlogDetailView.as_view(), name='blog-detail'),
+    path('blog/create/', BlogCreateView.as_view(), name='blog-create'),
+    path('blog/<int:pk>/edit/', BlogUpdateView.as_view(), name='blog-edit'),
+    path('blog/<int:pk>/delete/', BlogDeleteView.as_view(), name='blog-delete'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,20 +2,23 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.request import Request
-from .models import Blog
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.shortcuts import get_object_or_404
+from django.urls import reverse_lazy
+
+from .models import Blog, Tag
 from .serializers import BlogSerializer
+from .forms import BlogForm
 
 class BlogAPIView(APIView):
-    def get(self, request: Request, pk: int = None) -> Response:
+    def get(self, request: Request, pk: int = None):
         if pk is None:
             blogs = Blog.objects.all()
             serializer = BlogSerializer(blogs, many=True)
             return Response(data=serializer.data, status=status.HTTP_200_OK)
 
         # get single blog
-        blog = Blog.objects.filter(id=pk).first()
-        if blog is None:
-            return Response(data={"error": "Blog Not Found"}, status=status.HTTP_404_NOT_FOUND)
+        blog = get_object_or_404(Blog, id=pk)
         serializer = BlogSerializer(blog)
         return Response(data=serializer.data, status=status.HTTP_200_OK)
 
@@ -27,9 +30,7 @@ class BlogAPIView(APIView):
         return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def put(self, request: Request, pk: int) -> Response:
-        blog = Blog.objects.filter(id=pk).first()
-        if blog is None:
-            return Response(data={"error": "Blog Not Found"}, status=status.HTTP_404_NOT_FOUND)
+        blog = get_object_or_404(Blog, id=pk)
 
         serializer = BlogSerializer(blog, data=request.data, partial=True)
         if serializer.is_valid():
@@ -38,9 +39,59 @@ class BlogAPIView(APIView):
         return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request: Request, pk: int) -> Response:
-        blog = Blog.objects.filter(id=pk).first()
-        if blog is None:
-            return Response(data={"error": "Blog not found"}, status=status.HTTP_404_NOT_FOUND)
-
+        blog = get_object_or_404(Blog, id=pk)
         blog.delete()
         return Response(data={"message": "Blog deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
+
+
+# Django HTML Views
+class BlogListView(ListView):
+    model = Blog
+    template_name = 'index.html'
+    context_object_name = 'blogs'
+
+
+class BlogDetailView(DetailView):
+    model = Blog
+    template_name = 'blog_detail.html'
+    context_object_name = 'blog'
+
+
+class BlogCreateView(CreateView):
+    model = Blog
+    form_class = BlogForm
+    template_name = 'blog_form.html'
+    success_url = reverse_lazy('blog-list')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tags'] = Tag.objects.all()
+        return context
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        self.object.tags.set(self.request.POST.getlist('tags'))
+        return response
+
+
+class BlogUpdateView(UpdateView):
+    model = Blog
+    form_class = BlogForm
+    template_name = 'blog_form.html'
+    success_url = reverse_lazy('blog-list')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tags'] = Tag.objects.all()  # Include tags in the context
+        return context
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        self.object.tags.set(self.request.POST.getlist('tags'))
+        return response
+
+
+class BlogDeleteView(DeleteView):
+    model = Blog
+    template_name = 'blog_confirm_delete.html'
+    success_url = '/'
